e22a2888b68de872f9b19734634a4182493c6bc1
awkward crow <awkward.crow@gmail.com>
2023-11-12 12:12:28 +0000

    introduce CategoricalSummary object and show method

--- summary.jl
+++ summary.jl
@@ -1,20 +1,32 @@
 # summary.jl -- a module in the making
 
+
+using CategoricalArrays
+
+struct CategoricalSummary
+    levels
+    counts
+end
+
 function justify(s, n)
     repeat(" ", n - length(s)) * s
 end
 
-function summary(x)
-    v = levels(x)
-    counts = map(v -> sum(x .== v), levels(x))
-    n = string.(counts)
-    k = maximum(vcat(length.(v), length.(n))) + 1
-    for u in v
-        print(justify(u, k))
+function summary(x::CategoricalVector)
+    CategoricalSummary(levels(x), map(v -> sum(x .== v), levels(x)))
+end
+
+import Base.show
+
+function show(io::IO, obj::CategoricalSummary)
+    counts = string.(obj.counts)
+    k = maximum(vcat(length.(obj.levels), length.(counts))) + 1
+    for v in obj.levels
+        print(justify(v, k))
     end
     println("")
-    for j in n
-        print(justify(j, k))
+    for n in counts
+        print(justify(n, k))
     end
 end
 
